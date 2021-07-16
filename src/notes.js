5a8c1061201992f05836965062ad8fa5645e8601
Lunnardo <lunnardolukias@gmail.com>
2021-07-16 22:57:48 +0700

    First commit

---
+++ src/notes.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-console */
+
+const notes = [];
+     
+module.exports = notes;
